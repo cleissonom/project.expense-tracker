6e9012e011a5fcc25f099eff21c7ad13f8940c72
Cleisson <cleissonconstc@gmail.com>
2021-07-17 16:31:23 -0300

    :tada: first commit

---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+// import React from 'react'
+
+// export const Header = () => {
+//    return (
+//       <h2>
+//          Expense Tracker
+//       </h2>
+//    )
+// }
+
+
+import React from 'react'
+
+export function Header() {
+   return (
+      <div>
+         Expense Tracker
+      </div>
+   )
+}
+
